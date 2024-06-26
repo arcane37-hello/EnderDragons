73865ed98c3a682f4cca76d5c561e8c22aee2c83
arcane37-hello <unitypractice37@gmail.com>
2024-06-26 17:48:45 +0900

    생성되는 땅 y 좌표 추가

--- Assets/PSH/Scripts/GroundGenerator.cs
+++ Assets/PSH/Scripts/GroundGenerator.cs
@@ -6,6 +6,7 @@ public class GroundGenerator : MonoBehaviour
 {
     public GameObject cubePrefab;
     public int groundWidth = 100; // x 방향 큐브 개수
+    public int groundHeight = 3; // y 방향 큐브 개수
     public int groundLength = 100; // z 방향 큐브 개수
 
     void Start()
@@ -17,19 +18,22 @@ public class GroundGenerator : MonoBehaviour
     {
 
         // 큐브들을 배치할 시작 위치 설정
-        Vector3 startPosition = new Vector3(-groundWidth / 2, 0, -groundLength / 2);
+        Vector3 startPosition = new Vector3(-groundWidth / 2, groundHeight / 2, -groundLength / 2);
 
         // 땅을 구성할 큐브들을 생성하는 코드
         for (int x = 0; x < groundWidth; x++)
         {
-            for (int z = 0; z < groundLength; z++)
+            for (int y = 0; y < groundHeight; y++)
             {
-                // 각 큐브의 위치 계산
-                Vector3 position = startPosition + new Vector3(x, 0, z);
+                for (int z = 0; z < groundLength; z++)
+                {
+                    // 각 큐브의 위치 계산
+                    Vector3 position = startPosition + new Vector3(x, y, z);
 
-                // 큐브 인스턴스 생성
-                GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
-                cube.transform.SetParent(transform); // 땅 생성기(GroundGenerator)의 자식으로 설정
+                    // 큐브 인스턴스 생성
+                    GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
+                    cube.transform.SetParent(transform); // 땅 생성기(GroundGenerator)의 자식으로 설정
+                }
             }
         }
     }

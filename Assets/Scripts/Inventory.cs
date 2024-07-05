053309e6044e0f0340f62498a53b32fe3493fb76
Jogatun <jogatun0108@gmail.com>
2024-07-05 15:38:20 +0900

    ESC 버그 수정

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -59,56 +59,89 @@ public class Inventory : MonoBehaviour
 
 
         }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isblackbackground = !isblackbackground;
 
+            if (isblackbackground)
+            {
+               
+                if (blackbackground != null)
+                {
+                    blackbackground.SetActive(true); // 배경 활성화
+                }
+               
+                Cursor.lockState = CursorLockMode.None; // 마우스 커서 활성화
+                Cursor.visible = true;
+                //Debug.Log("Game Paused");
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
 
         //if (Input.GetKeyDown(KeyCode.E))
         //    InventoryUI();
 
     }
-//    public void InventoryUI
-//    {
-//         if (Input.GetKeyDown(KeyCode.E))
-//        {
-//            if(isInventoryOpen)
-//            Inventory.SetActive(true);
-//}
+    public void ResumeGame()
+    {
+        isblackbackground = false;
+        Time.timeScale = 1f; // 게임 시간을 재개
+        if (blackbackground != null)
+        {
+            blackbackground.SetActive(false); // 배경 비활성화
+        }
+       
+        Cursor.lockState = CursorLockMode.Locked; // 마우스 커서 비활성화
+        Cursor.visible = false;
+        //Debug.Log("Game Resumed");
+    }
+    //    public void InventoryUI
+    //    {
+    //         if (Input.GetKeyDown(KeyCode.E))
+    //        {
+    //            if(isInventoryOpen)
+    //            Inventory.SetActive(true);
+    //}
 
-//    }
+    //    }
 
 
     // 게임 오버 시 실행함수
-    public void ShowGameOverUI()
-    {
-        // 게임 오버 UI 오브젝트를 활성화한다.
-        GameOverUI.SetActive(true);
+    //    public void ShowGameOverUI()
+    //    {
+    //        // 게임 오버 UI 오브젝트를 활성화한다.
+    //        GameOverUI.SetActive(true);
 
-        // 업데이트 시간을 0배속으로 변경한다. (시간을 멈춤)
-        Time.timeScale = 0f;
-    }
-    // 계속하기 시에 실행함수
-    public void ContinueGame()
-    {
-        GameOverUI.SetActive(false);
-        Time.timeScale = 1.0f;
-    }
-    // 게임을 처음부터 다시 시작하는 함수
-    public void RestartGame()
-    {
-        // 업데이트 시간을 다시 1배율로 변경한다.
-        Time.timeScale = 1.0f;
+    //        // 업데이트 시간을 0배속으로 변경한다. (시간을 멈춤)
+    //        Time.timeScale = 0f;
+    //    }
+    //    // 계속하기 시에 실행함수
+    //    public void ContinueGame()
+    //    {
+    //        GameOverUI.SetActive(false);
+    //        Time.timeScale = 1.0f;
+    //    }
+    //    // 게임을 처음부터 다시 시작하는 함수
+    //    public void RestartGame()
+    //    {
+    //        // 업데이트 시간을 다시 1배율로 변경한다.
+    //        Time.timeScale = 1.0f;
 
-        // 현재 씬 다시 재생
-        SceneManager.LoadScene("PlayerScene");
-    }
-    // 어플리케이션을 종료하는 함수
-    public void QuitGame()
-    {
-#if UNITY_EDITOR
-        // 1. 에디터의 경우
-        EditorApplication.ExitPlaymode();
-#elif UNITY_STANDALONE
-        // 2. 어플리케이션 경우
-        Application.Quit();
-#endif
-    }
+    //        // 현재 씬 다시 재생
+    //        SceneManager.LoadScene("PlayerScene");
+    //    }
+    //    // 어플리케이션을 종료하는 함수
+    //    public void QuitGame()
+    //    {
+    //#if UNITY_EDITOR
+    //        // 1. 에디터의 경우
+    //        EditorApplication.ExitPlaymode();
+    //#elif UNITY_STANDALONE
+    //        // 2. 어플리케이션 경우
+    //        Application.Quit();
+    //#endif
+    //    }
 }

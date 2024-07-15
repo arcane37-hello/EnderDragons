eca07e7231c9bc082ad0798c510483b9d22bfef7
arcane37-hello <unitypractice37@gmail.com>
2024-07-15 10:20:35 +0900

    포탈 구현

---
+++ Assets/PSH/Scripts/BossPortal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossPortal : MonoBehaviour
+{
+    // 충돌을 감지할 트리거 설정
+    private void OnTriggerEnter(Collider other)
+    {
+        // 충돌한 오브젝트의 태그가 "Player"인지 확인
+        if (other.CompareTag("Player"))
+        {
+            // 씬 전환: "2"번 씬으로 이동
+            SceneManager.LoadScene(2);  // 2는 씬의 인덱스입니다. 
+        }
+    }
+
+    // 트리거를 활성화하려면 해당 콜라이더의 "Is Trigger"를 체크해야 합니다.
+}
